c226450fb5b06181cfecefce1e079d6279c4c931
byhat <goggssg3@mail.ru>
2025-12-06 23:41:06 +0300

    mvp version

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include "presentation/bridge/AppEngine.hpp"
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication app(argc, argv);
+
+    AppEngine appEngine;
+
+    QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("appEngine", &appEngine);
+
+    const QUrl url("qrc:/AppQml/presentation/qml/Main.qml");
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
+                     &app, [url](QObject *obj, const QUrl &objUrl) {
+                         if (!obj && url == objUrl)
+                             QCoreApplication::exit(-1);
+                     }, Qt::QueuedConnection);
+    engine.load(url);
+
+    return app.exec();
+}
