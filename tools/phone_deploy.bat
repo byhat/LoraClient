f904bebdb208e13a2f15ed0e8567f332b5985eb4
byhat <goggssg3@mail.ru>
2026-03-02 13:06:33 +0300

    update android build

--- tools/phone_deploy.bat
+++ tools/phone_deploy.bat
@@ -1,2 +1,2 @@
-C:/Users/User/AppData/Local/Android/Sdk/platform-tools/adb.exe install -r build\android-build-debug.apk
-C:/Users/User/AppData/Local/Android/Sdk/platform-tools/adb.exe shell am start -n org.qtproject.example.lora_app/org.qtproject.qt.android.bindings.QtActivity
+C:/Users/Antonov/AppData/Local/Android/Sdk/platform-tools/adb.exe install -r build\android-build-debug.apk
+C:/Users/Antonov/AppData/Local/Android/Sdk/platform-tools/adb.exe shell am start -n org.loraclient.app/org.qtproject.qt.android.bindings.QtActivity
